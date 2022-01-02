e04fc9a8d5fc300318436816134e6b1a4b30878a
Matthew Finch <finchrmatthew@gmail.com>
2022-01-01 19:06:10 -0500

    commonCharacterCount

---
+++ javascript/commonCharacterCount.js
@@ -0,0 +1,23 @@
+/*
+
+Given two strings, find the number of common characters between them.
+
+*/
+
+
+function solution(s1, s2) {
+
+    let count = 0;
+    
+    for(let i = 0; i < s1.length; i++) {
+        
+        let j = s2.indexOf(s1[i]);
+        
+        if(j>=0){
+            count++;
+            s2 = s2.replace(s1[i],'');
+        }
+    }
+    return count;
+
+}
